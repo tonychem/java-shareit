a0289a345ec0b9d3b7732e95b9ac623d58914831
Anatoly <tonychem@yandex.ru>
2022-08-14 19:44:00 +0300

    добавить мапперы для dto

---
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
@@ -0,0 +1,26 @@
+package ru.practicum.shareit.item.dto;
+
+import ru.practicum.shareit.item.model.Item;
+
+public class ItemMapper {
+
+    public static ItemDto toItemDto(Item item) {
+        return new ItemDto(item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.isAvailable(),
+                item.getOwner(),
+                item.getRequest()
+        );
+    }
+
+    public static Item toItem(ItemDto itemDto) {
+        return new Item(itemDto.getId(),
+                itemDto.getName(),
+                itemDto.getDescription(),
+                itemDto.isAvailable(),
+                itemDto.getOwner(),
+                itemDto.getRequest()
+        );
+    }
+}
