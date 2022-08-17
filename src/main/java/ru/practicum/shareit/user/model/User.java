3f6bac6620a0cc669e62228ca48c6d63a3f2c58a
Anatoly <tonychem@yandex.ru>
2022-08-17 21:35:38 +0300

    style: убрать валидацию из моделей

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -2,21 +2,11 @@ package ru.practicum.shareit.user.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
-import org.springframework.validation.annotation.Validated;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
 public class User {
     private long id;
-    @NonNull
     private String name;
-    @Email
-    @NotNull
-    @NotEmpty
     private String email;
 }
