4ead2f76e5bb96fca8a6a7bbd94bbcdc677f9efb
Anatoly <tonychem@yandex.ru>
2022-09-06 21:32:34 +0300

    style: fix style

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -2,14 +2,11 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.annotation.PreDestroy;
 
 @SpringBootApplication
 public class ShareItApp {
-	public static void main(String[] args) {
-		SpringApplication.run(ShareItApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItApp.class, args);
+    }
 
 }
