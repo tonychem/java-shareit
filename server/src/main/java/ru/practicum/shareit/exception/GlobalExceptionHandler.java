f7a27d42e5f36324abc05b943c8744abccdf2005
Anatoly <tonychem@yandex.ru>
2022-12-03 13:16:30 +0300

    style: убрать неиспользуемые импорты

--- server/src/main/java/ru/practicum/shareit/exception/GlobalExceptionHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/GlobalExceptionHandler.java
@@ -3,8 +3,6 @@ package ru.practicum.shareit.exception;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
